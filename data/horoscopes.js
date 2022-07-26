1888fca8a1592678a79bba753bbf5cca813ac9e7
David <Rigby335@hotmail.com>
2022-07-26 11:34:26 -0700

    undid too far, had to fix path in app.js

--- data/horoscopes.js
+++
@@ -1,6 +0,0 @@
-// const horoscopes = [
-//   {
-//     'date': '2022-07-26',
-//     'horoscope': 'The Cancer moon aligns with Venus in the sector of your chart that governs wellness this morning, dear Aquarius, encouraging you to kick off the day with healthy habits. Be sure to hydrate, eat well, and get some stretching in, and the universe will smile on you as the hours continue to pass. Unfortunately, a hard square between Mercury and Mars could cause you to feel more emotional than usual, especially if your love life has been a bit rocky as of late. Try not to hang your happiness on someone else right now, and don\'t feel guilty if you need to take some time out for yourself.'
-//   }    
-// ]
